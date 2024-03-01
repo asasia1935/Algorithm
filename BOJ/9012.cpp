d043bfdf6d80beb304b413f6ca81f27abde415c7
asasia1935 <88706217+asasia1935@users.noreply.github.com>
2024-03-01 21:51:37 +0900

    Solve 9012

---
+++ BOJ/9012.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	
+	int k;
+	cin >> k;
+	
+	while (k > 0) {
+		k--;
+		string input;
+		cin >> input;
+
+		stack<char> st;
+		string answer = "YES";
+		for (int i = 0; i < input.length(); i++) {
+			if (input[i] == '(') {
+				st.push(input[i]);
+			}
+			else if (!st.empty() && input[i] == ')' && st.top() == '(') {
+				st.pop();
+			}
+			else {
+				answer = "NO";
+				break;
+			}
+		}
+		if (!st.empty()) answer = "NO";
+
+		cout << answer << endl;
+	}
+	return 0;
+}
