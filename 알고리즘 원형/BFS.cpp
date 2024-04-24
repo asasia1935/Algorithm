d32bdb818f029fc0d593675c6ee371c9a5e41c5b
asasia1935 <sungmin1935@naver.com>
2024-04-24 22:34:52 +0900

    [Rename] 기존 코드 위치 수정

---
+++ 알고리즘 원형/BFS.cpp
@@ -0,0 +1,68 @@
+// 코드 참고 : https://github.com/ndb796/python-for-coding-test
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+bool visited[9];
+vector<int> graph[9];
+
+// BFS 함수 정의
+void bfs(int start)
+{
+    queue<int> q;
+    q.push(start);         // 첫 노드를 queue에 삽입
+    visited[start] = true; // 첫 노드를 방문 처리
+
+    // 큐가 빌 때까지 반복
+    while (!q.empty())
+    {
+        // 큐에서 하나의 원소를 뽑아 출력
+        int x = q.front();
+        q.pop();
+        cout << x << ' ';
+        // 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for (int i = 0; i < graph[x].size(); i++)
+        {
+            int y = graph[x][i];
+            if (!visited[y])
+            {
+                q.push(y);
+                visited[y] = true;
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    graph[1].push_back(2);
+    graph[1].push_back(3);
+    graph[1].push_back(8);
+
+    graph[2].push_back(1);
+    graph[2].push_back(7);
+
+    graph[3].push_back(1);
+    graph[3].push_back(4);
+    graph[3].push_back(5);
+
+    graph[4].push_back(3);
+    graph[4].push_back(5);
+
+    graph[5].push_back(3);
+    graph[5].push_back(4);
+
+    graph[6].push_back(7);
+
+    graph[7].push_back(2);
+    graph[7].push_back(6);
+    graph[7].push_back(8);
+
+    graph[8].push_back(1);
+    graph[8].push_back(7);
+
+    bfs(1);
+}
