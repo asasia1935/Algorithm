5ac52987773333d8860f60788a84a850457e139e
asasia1935 <88706217+asasia1935@users.noreply.github.com>
2025-11-13 19:17:29 +0900

    [Bronze II] Title: 단어의 개수, Time: 28 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+    int count = 0;
+    if(str.empty()) {
+        cout << count;
+        return 0;
+    }
+    count++;
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == ' ')
+            count++;
+    }
+    if (str[0] == ' ')
+        count--;
+    if (str[str.length() - 1] == ' ')
+        count--;
+    cout << count;
+    return 0;
+}
