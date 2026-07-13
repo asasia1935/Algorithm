e448d24b1c4b1699b3214b439ef437d62fcff3b9
asasia1935 <88706217+asasia1935@users.noreply.github.com>
2026-07-13 22:28:37 +0900

    [Passed] 국영수 순이지 (2ms, 1MB)

--- trail2/국영수 순이지/korean-english-math-order.cpp
+++ trail2/국영수 순이지/korean-english-math-order.cpp
@@ -11,33 +11,48 @@ int korean[10];
 int english[10];
 int math[10];
 
+struct Person {
+    string name;
+    int korean;
+    int english;
+    int math;
+};
+
+bool cmp(const Person& a, const Person& b) {
+    if (a.korean != b.korean) {
+        return a.korean > b.korean;
+    }
+
+    if (a.english != b.english) {
+        return a.english > b.english;
+    }
+
+    return a.math > b.math;
+}
+
 int main() {
     cin >> n;
+
+    vector<Person> people(n);
 
     for (int i = 0; i < n; i++) {
         cin >> name[i];
         cin >> korean[i];
         cin >> english[i];
         cin >> math[i];
+
+        people[i].name = name[i];
+        people[i].korean = korean[i];
+        people[i].english = english[i];
+        people[i].math = math[i];
     }
 
-    vector<tuple<int, int, int, string>> people(n);
-
-    for (int i = 0; i < n; i++) {
-        people[i] = {-korean[i], -english[i], -math[i], name[i]};
-    }
-
-    sort(people.begin(), people.end());
+    sort(people.begin(), people.end(), cmp);
 
     // Please write your code here.
 
     for (int i = 0; i < n; i++) {
-        string nam;
-        int kor, eng, mat;
-
-        tie(kor, eng, mat, nam) = people[i];
-
-        cout << nam << " " << -kor << " " << -eng << " " << -mat << "\n";
+        cout << people[i].name << " " << people[i].korean << " " << people[i].english << " " << people[i].math << "\n";
     }
 
     return 0;
