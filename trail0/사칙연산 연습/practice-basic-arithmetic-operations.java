212ff7f2d75472d84bfcd238ba238d552d01185b
asasia1935 <88706217+asasia1935@users.noreply.github.com>
2026-08-02 23:21:58 +0900

    [Passed] 사칙연산 연습 (86ms, 9MB)

---
+++ trail0/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 30;
+
+        int c = a + b;
+        int q = c / 8;
+        int r = c % 8;
+
+        System.out.println(q);
+        System.out.println(r);
+    }
+}
