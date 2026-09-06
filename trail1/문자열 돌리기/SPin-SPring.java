3474cc63ddf75680b103033eabc36d837bc4fc9e
asasia1935 <88706217+asasia1935@users.noreply.github.com>
2026-09-06 22:29:05 +0900

    [Passed] 문자열 돌리기 (123ms, 10MB)

---
+++ trail1/문자열 돌리기/SPin-SPring.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        for(int i = 0; i < str.length() + 1; i++) {
+            System.out.println(str);
+            str = str.substring(str.length() - 1) + str.substring(0, str.length() - 1);
+        }
+    }
+}
